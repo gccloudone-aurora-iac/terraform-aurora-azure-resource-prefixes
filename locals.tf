678ecda1e179b557700f37350905677b57e64d47
Sajid Hafiz <sajid.hafiz@cloud.statcan.ca>
2025-02-10 17:47:23 -0700

    naming convention

--- locals.tf
+++ locals.tf
@@ -3,35 +3,84 @@ locals {
   location = lookup(local.location_table, replace(lower(var.name_attributes.location), " ", ""))
   instance = format("%02s", var.name_attributes.instance)
 
-  common_convention_base = "${var.name_attributes.project}-${var.name_attributes.environment}-${local.location}-${local.instance}"
-}
+  common_convention_base_statcan = "${var.name_attributes.project}-${var.name_attributes.environment}-${local.location}-${local.instance}"
+  common_convention_base_ssc     = "${var.name_attributes_ssc.department_code}${var.name_attributes_ssc.environment}${var.name_attributes_ssc.csp_region}"
 
-locals {
-  location_table = {
-    "canadacentral" = "cc"
-    "canadaeast"    = "ce"
+  locals {
+    location_table = {
+      "canadacentral" = "cc"
+      "canadaeast"    = "ce"
+    }
+
+    resource_type_abbreviations_statcan = {
+      "application security group"     = "asg"
+      "disk encryption set"            = "des"
+      "firewall"                       = "fw"
+      "kubernetes service"             = "aks"
+      "load balancer"                  = "lb"
+      "network interface card"         = "nic"
+      "network security group"         = "nsg"
+      "private endpoint"               = "pe"
+      "public ip address"              = "pip"
+      "resource group"                 = "rg"
+      "route server"                   = "rs"
+      "route table"                    = "rt"
+      "service endpoint policy"        = "sep"
+      "service principal"              = "sp"
+      "user-assigned managed identity" = "msi"
+      "virtual machine"                = "vm"
+      "virtual machine scale set"      = "vmss"
+      "virtual network"                = "vnet"
+    }
+
+    resource_type_abbreviations_ssc = {
+      "application security group"     = "asg"
+      "disk encryption set"            = "des"
+      "firewall"                       = "fw"
+      "kubernetes service"             = "aks"
+    # "load balancer"                  = "lb"
+      "network interface card"         = "nic"
+      "network security group"         = "nsg"
+      "private endpoint"               = "pe"
+      "public ip address"              = "pip"
+      "resource group"                 = "rg"
+      "route server"                   = "rs"
+      "route table"                    = "rt"
+      "service endpoint policy"        = "sep"
+      "service principal"              = "sp"
+      "user-assigned managed identity" = "msi"
+      "virtual machine"                = "vm"
+      "virtual machine scale set"      = "vmss"
+      "virtual network"                = "vnet"
+      # "azure data factory"             = "adf"
+      # "databricks service"             = "dbw"
+      # "web application"                = "wapp"
+      # "application insights"           = "appi"
+      # "azure bastion end-point"        = "bstn"
+      # "network watcher"                = "nw"
+      # "vnet peering"                   = "gwp"
+      # "local network gateway"          = "lgn"
+      # "virtual network gateway"        = "vng"
+      # "load balancer backend pool"     = "lbbp"
+      # "load balancer health probe"     = "lbhp"
+      # "load balancer rule"             = "lbr"
+      # "azure load balancer"            = "alb"
+      # "traffic manager"                = "tm"
+      # "storage account"                = "stg"
+      # "subnet"                         = "snet"
+    }
+
+    common_conv_prefixes_statcan = {
+      for resource_type, abbrev in local.resource_abbreviations_statcan :
+      resource_type => "${local.convention_base_statcan}-${abbrev}"
+    }
+
+    common_conv_prefixes_ssc = {
+      for resource_type, abbrev in local.resource_abbreviations_ssc :
+      resource_type => "${local.common_convention_base_ssc}-${var.user_defined}-${abbrev}"
+    }
+
+    common_conv_prefixes = var.government ? common_conv_prefixes_ssc : common_conv_prefixes_statcan
+
   }
-
-  resource_type_abbreviations = {
-    "application security group"     = "asg"
-    "disk encryption set"            = "des"
-    "firewall"                       = "fw"
-    "kubernetes service"             = "aks"
-    "load balancer"                  = "lb"
-    "network interface card"         = "nic"
-    "network security group"         = "nsg"
-    "private endpoint"               = "pe"
-    "public ip address"              = "pip"
-    "resource group"                 = "rg"
-    "route server"                   = "rs"
-    "route table"                    = "rt"
-    "service endpoint policy"        = "sep"
-    "service principal"              = "sp"
-    "user-assigned managed identity" = "msi"
-    "virtual machine"                = "vm"
-    "virtual machine scale set"      = "vmss"
-    "virtual network"                = "vnet"
-  }
-
-  common_conv_prefixes = { for resource_type, abbrev in local.resource_type_abbreviations : resource_type => "${local.common_convention_base}-${abbrev}" }
 }
--- variables.tf
+++ variables.tf
@@ -27,8 +27,52 @@ variable "name_attributes" {
   }
 }
 
+variable "name_attributes_ssc" {
+  type = object({
+    department_code = string
+    csp_region          = string
+    environment     = string
+  })
+
+  validation {
+    condition     = length(var.name_attributes_ssc.environment) == 1
+    error_message = "Environment must be 1 character long"
+  }
+
+  validation {
+    condition     = length(var.name_attributes_ssc.department_code) == 2
+    error_message = "Department code must be 2 characters long"
+  }
+
+  validation {
+    condition     = length(var.name_attributes_ssc.region) == 1
+    error_message = "Region must be 1 character long"
+  }
+
+  validation {
+    condition = var.government
+    error_message = "The government variable is required to be true when setting the name_attributes_ssc variable."
+  }
+}
+
+variable "user_defined" {
+  description = "A user-defined field that describes the Azure resource."
+  type        = string
+  nullable    = false
+
+  validation {
+    condition     = length(var.user_defined) >= 2 && length(var.user_defined) <= 15
+    error_message = "The user-defined field must be between 2-15 characters long."
+  }
+
+  validation {
+    condition = var.government
+    error_message = "The government variable is required to be true when setting the user_defined variable."
+  }
+}
+
 variable "government" {
-  type = bool
-  default = false
+  type        = bool
+  default     = false
   description = "Sets which naming convention to use. If true, use SSC's otherwise use StatCan's."
 }
